9b8e0d6c2bcc79af135a40581484ea8128ec15e9
AndabeleGit <Andabele10@hotmail.con>
2024-09-09 17:13:02 -0500

    filtros extracto incompleto

---
+++ Pruebas_Individuales_Logica/Extractos/Filtrar_Extractos.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from db_config import obtener_conexion
+from mysql.connector import Error
+
+try:
+    conexion = obtener_conexion()
+    if conexion:
+        cursor = conexion.cursor()
+
+# upper en todos los nombres
+# lower en todas las descripciones       
+# upper en todos los salida_dinero
+# upper en todos los entrada_dinero
+# Filtro por nombre, descripcion, salida_dinero, fecha_de_creacion
+        
+except Error as e:
+    print(f"Error: {e}")
+except ValueError as ve:
+    print(f"Advertencia: {ve}")
+finally:
+    if conexion and conexion.is_connected():
+        cursor.close()
+        conexion.close()
+        print("Conexión a MySQL cerrada.")
