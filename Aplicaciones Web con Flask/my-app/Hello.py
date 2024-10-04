885d624c993ce72db3f43de9a432a9422b62fe7a
AndabeleGit <Andabele10@hotmail.con>
2024-10-04 07:40:42 -0500

    commit

--- Aplicaciones Web con Flask/my-app/Hello.py
+++ Aplicaciones Web con Flask/my-app/Hello.py
@@ -3,5 +3,17 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def Hello():
-    return 'Hola Mundo'
+def index():
+    return '<h1>Pagina de inicio</h1>'
+
+
+@app.route('/Hello')
+@app.route('/Hello/<name>')
+@app.route('/Hello/<name>/<int:age>')
+def Hello(name = None, age = None):
+    if name == None and age == None:
+        return "<h1>Hola Mundo</h1>"
+    elif age == None:
+        return f"<h1>Hola {name}"
+    else:
+        return f'Hola {name}, y tu edad es {age}'
